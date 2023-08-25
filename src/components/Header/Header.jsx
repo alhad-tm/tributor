c77ce62b5e98eee0ba5021ecee247dbc99d1c80d
alhad-tm <alhadchikkutm@gmail.com>
2023-08-25 15:59:52 +0530

    main page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import css from "./Header.module.css"
 
 const Header = () => {
   return (
-    <div>
+    <div className={css.container}>
         hi
       
     </div>
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import css from "./Main.module.css"
+import Tb1 from "../../assets/tributor1.png"
+
+const Main = () => {
+  return (
+    <div className={css.container}>
+        <div className={css.div1}>
+            <img src={Tb1} alt="" />
+        </div>
+
+        <div className={css.div2}>
+            <span>Crafted with Passion and Precision</span>
+        </div>
+        <div className={css.div3}>
+            <span>Tributor stands as your all-inclusive destination for flawless film distribution and content management, coupled with a cutting-edge movie ticket booking app.</span>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Main 
+  
