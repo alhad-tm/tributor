25ce72c8cc8b3eb37422a28473e11300124f9aff
alhad-tm <alhadchikkutm@gmail.com>
2023-08-28 19:45:30 +0530

    added font

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import css from "./Main.module.css"
 import Tb1 from "../../assets/tributor1.svg"
+import Darrow from "../../assets/downarrow.svg"
 
 const Main = () => {
   return (
@@ -15,6 +16,11 @@ const Main = () => {
         <div className={css.div3}>
             <span>Tributor stands as your all-inclusive destination for flawless film distribution and content management, coupled with a cutting-edge movie ticket booking app.</span>
         </div>
+
+        <div className={css.div4}>
+          <span>Our Products</span>
+          <img src={Darrow} alt="" />
+           </div>
       
     </div>
   )
